dc8ad74680f0d9681663402fa8ae014ae15d19a0
Vinicius Torves <vinicius@torves.com>
2014-07-05 01:05:20 -0300

    news routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
+   root 'welcome#index'
+
+  resources :welcome do 
+    get 'index'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
