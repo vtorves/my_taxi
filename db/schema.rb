61a606ac7d6d6f3e62af53ca80e0d9024af5f2ca
Vinicius Torves <vinicius@torves.com>
2014-07-05 01:05:07 -0300

    add new model Way and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140705022425) do
+ActiveRecord::Schema.define(version: 20140705040121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,5 +35,12 @@ ActiveRecord::Schema.define(version: 20140705022425) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "ways", force: true do |t|
+    t.string   "name"
+    t.boolean  "situation"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
